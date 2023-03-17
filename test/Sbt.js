68e497754609441cd1b27fe5eb128a2218aa3550
witehound <netvid00@gmail.com>
2023-03-17 12:11:04 +0100

    ddebugging meta transaction test

--- test/Sbt.js
+++ test/Sbt.js
@@ -4,6 +4,8 @@ const {
 } = require("@nomicfoundation/hardhat-network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const DEPLOYERABI = require("../artifacts/contracts/Sbt.sol/SBTDeployer.json");
 
 describe("Sbt", function () {
   async function deploySbtDeployer() {
@@ -14,17 +16,29 @@ describe("Sbt", function () {
     const amount = ONE_GWEI;
     const [owner, acc1, acc2] = await ethers.getSigners();
     const SbtDeployer = await ethers.getContractFactory("SBTDeployer");
+    const TestTokens = await ethers.getContractFactory("ERC20_Token_Sample");
     const sbtDeployer = await SbtDeployer.deploy(
       SIGNING_DOMAIN,
       SIGNATURE_VERSION
     );
 
-    return { owner, sbtDeployer, acc1, acc2, amount };
+    const testTokens = await TestTokens.deploy();
+
+    return {
+      owner,
+      sbtDeployer,
+      acc1,
+      acc2,
+      amount,
+      testTokens,
+      SIGNING_DOMAIN,
+      SIGNATURE_VERSION,
+    };
   }
 
   describe("Deploy new sbt", function () {
-    it("should not fail", async function () {
-      const { owner, sbtDeployer, amount, acc1, acc2 } = await loadFixture(
+    it("test regular deployment", async function () {
+      const { sbtDeployer, amount, acc1, testTokens } = await loadFixture(
         deploySbtDeployer
       );
 
@@ -32,11 +46,75 @@ describe("Sbt", function () {
         "first",
         "FST",
         amount,
+        testTokens.address,
         acc1.address,
-        acc2.address,
         false
       );
-      //   console.log("first", await sbtDeployer.getContractByIndex(0));
+
+      expect(await sbtDeployer.getContractByIndex(0)).to.not.equal(0x0);
+    });
+
+    it("test meta transaction deployment", async function () {
+      const {
+        sbtDeployer,
+        SIGNATURE_VERSION,
+        SIGNING_DOMAIN,
+        acc2,
+        amount,
+        testTokens,
+        acc1,
+        owner,
+      } = await loadFixture(deploySbtDeployer);
+      const domainType = [
+        { name: "name", type: "string" },
+        { name: "version", type: "string" },
+        { name: "verifyingContract", type: "address" },
+        { name: "salt", type: "bytes32" },
+      ];
+      const metaTransactionType = [
+        { name: "nonce", type: "uint256" },
+        { name: "from", type: "address" },
+        { name: "functionSignature", type: "bytes" },
+      ];
+
+      let domainData = {
+        name: SIGNING_DOMAIN,
+        version: SIGNATURE_VERSION,
+        verifyingContract: sbtDeployer.address,
+        // salt: "0x" + (42).toString(16).padStart(64, "0"),
+        salt: "0x0000000000000000000000000000000000000000000000000000000000007a69",
+      };
+
+      let nonce = await sbtDeployer.getNonce(acc2.address);
+
+      const contractInterface = new ethers.utils.Interface(DEPLOYERABI.abi);
+      let functionSignature = contractInterface.encodeFunctionData(
+        "deployNewSBT",
+        ["first", "FST", amount, testTokens.address, acc1.address, false]
+      );
+
+      let message = {
+        nonce: parseInt(nonce),
+        from: owner.address,
+        functionSignature: functionSignature,
+      };
+
+      const dataToSign = JSON.stringify({
+        types: {
+          EIP712Domain: domainType,
+          MetaTransaction: metaTransactionType,
+        },
+        domain: domainData,
+        primaryType: "MetaTransaction",
+        message: message,
+      });
+
+      const provider = ethers.provider;
+
+      let signature = await provider.send("eth_signTypedData_v3", [
+        owner.address,
+        dataToSign,
+      ]);
     });
   });
 });
