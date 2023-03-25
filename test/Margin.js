8c2f2c2931ae3aa76ae20cdf11d87560f3104fb4
witehound <netvid00@gmail.com>
2023-03-25 17:46:29 +0100

    all test cases pass

--- test/Margin.js
+++ test/Margin.js
@@ -30,5 +30,38 @@ describe("Margin", function () {
 
       expect(tradeVolume.toNumber()).to.equal(0);
     });
+
+    it("Should all have values", async function () {
+      const { margin, owner } = await loadFixture(deployLinkedList);
+      let initialVolume = 100;
+      let finalVolume = 150;
+
+      expect(await margin.trade(1, initialVolume)).not.to.be.reverted;
+
+      let tradeslength = await margin.tradeslength(owner.address);
+
+      expect(tradeslength.toNumber()).to.equal(1);
+      let instrument = await margin.instruments(owner.address, tradeslength);
+
+      expect(instrument.toNumber()).to.equal(1);
+
+      let tradeVolume = await margin.tradesVolume(owner.address, instrument);
+
+      expect(tradeVolume.toNumber()).to.equal(initialVolume);
+
+      expect(await margin.trade(1, finalVolume)).not.to.be.reverted;
+
+      let tradeslength2 = await margin.tradeslength(owner.address);
+
+      expect(tradeslength2.toNumber()).to.equal(1);
+
+      let instrument2 = await margin.instruments(owner.address, tradeslength);
+
+      expect(instrument2.toNumber()).to.equal(1);
+
+      let tradeVolume2 = await margin.tradesVolume(owner.address, instrument);
+
+      expect(tradeVolume2.toNumber()).to.equal(initialVolume + finalVolume);
+    });
   });
 });
